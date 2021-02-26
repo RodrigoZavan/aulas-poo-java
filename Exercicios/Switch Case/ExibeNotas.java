3b36a6fe2a23770f0598aa7650340b8125e6c4b9
RodrigoZavan <57202442+RodrigoZavan@users.noreply.github.com>
2021-02-26 19:41:17 -0300

    Add files via upload

---
+++ Exercicios/Switch Case/ExibeNotas.java
@@ -0,0 +1,15 @@
+package switchcase;
+
+public class ExibeNotas {
+
+	public static void main(String[] args) {
+		// Instanciar objeto
+		
+		Notas nota = new Notas(); 
+		
+		nota.entraNota();
+		nota.imprimeNota();
+
+	}
+
+}
